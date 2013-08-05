868c5bdf33740dea46c4e86679c2da4a14534740
Chris Tremonte <tremonte@gmail.com>
2013-08-05 14:04:08 -0700

    initializing repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IvfTracker::Application.config.secret_token = 'acd566ac74e254f870f387e56502cef4a8cc2a50aca92bb9859475e639ca7bdd09d77fe37b5fcea674ed46b0a1bd7e08ad1924d6c5e7e1f63c98b716a168a8f4'
